09ce1d76340888664bf6a5b8a83724accf2c564e
Adrian Medina <adrian@wikipunk.net>
2023-03-06 12:42:57 -0500

    update default dev system with smaller :xtdb node config

--- resources/aamedina/vocab/root/dev/dev.clj
+++ resources/aamedina/vocab/root/dev/dev.clj
@@ -19,15 +19,15 @@
    [com.stuartsierra.component :as com]
    [com.stuartsierra.component.repl :refer [reset set-init start stop system]]
    [com.walmartlabs.schematic :as sc]
-   [net.wikipunk.boot :as boot]
+   [net.wikipunk.boot]
+   [net.wikipunk.ext]
    [net.wikipunk.chat :as chat]
-   [net.wikipunk.ext :as ext]
    [net.wikipunk.mop :as mop :refer [isa? descendants parents ancestors]]
    [net.wikipunk.openai :as openai]
    [net.wikipunk.rdf :as rdf :refer [doc]]
    [net.wikipunk.temple :as temple]
    [zprint.core :as zprint]
-   [{{top/ns}}.{{main/ns}}.boot]
+   [{{top/ns}}.{{main/ns}}.boot :as boot]
    [{{top/ns}}.rdf.{{rdfa/prefix}}])
   (:refer-clojure :exclude [isa? descendants parents ancestors]))
 
